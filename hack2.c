9018c594103f5a4bc8b5254bf0728170c7e29a9c
William Kuhfahl <wkuhfahl2@unl.edu>
2022-02-04 19:34:18 +0000

    initial commit for hack2 repo

---
+++ hack2.c
@@ -0,0 +1,22 @@
+/**
+ * Author: William Kuhfahl
+ * Partner: Grant Meacham
+ * Date: 2/4/2022
+ * 
+ * bing bong, this program should take the latitude and
+ * and longitude from the user and output the distance
+ * between the two positions.
+ */
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <math.h>
+ 
+ int main(int argc, char **argv) {
+     
+     double longitude1, latitude1, longitude2, latitude2;
+
+     
+     
+ }
+ 
